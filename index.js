7a4d45e7bbf3cc4b864eb2852cf362a3f214c744
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-03 15:34:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges(array) {
   for(let i = 0; i < array.length - 1; i++) {
-    console.log("Welcome ${}")
+    console.log("Welcome ${array[i]}")
   }
 }
